451d1d830b583666657f3caa076f229e28423b32
joaquin171993 <119981614+jjsm-1993@users.noreply.github.com>
2023-03-22 22:29:49 -0600

    Update 22-3-2023 22:09 pm

--- Sales.API/Data/DataContext.cs
+++ Sales.API/Data/DataContext.cs
@@ -11,17 +11,24 @@ namespace Sales.API.Data
         }
 
 
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<City> Cities { get; set; }
         public DbSet<Country> Countries { get; set; }
         public DbSet<State> States { get; set; }
-        public DbSet<City> Cities { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            /*crear indices unicos*/
             //campo unico e irrepetible, ademas de colocarlo como indice por nombre, ya me lo ordena alfabeticamente, ahorra un proceso de ordenamiento
             modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+
+            //indices compuestos (se COLOCA con CountryId por ejm para expresar asi la composicion del indice compuesto, para que sea unico)
+            modelBuilder.Entity<State>().HasIndex("CountryId", "Name").IsUnique();
+            modelBuilder.Entity<City>().HasIndex("StateId", "Name").IsUnique();
+
         }
     }
 }
--- Sales.Shared/Entities/Country.cs
+++ Sales.Shared/Entities/Country.cs
@@ -10,5 +10,11 @@ namespace Sales.Shared.Entities
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
         [MaxLength(100, ErrorMessage = "El campo {0 no puede tener más de {1} caracteres}")]
         public string Name { get; set; } = null!;
+
+        public ICollection<State>? States { get; set; }
+
+        [Display(Name = "Estados/Departamentos")]
+        public int StatesNumber => States == null ? 0 : States.Count;
+
     }
 }
